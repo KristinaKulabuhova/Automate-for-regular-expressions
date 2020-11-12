26dbaa817ef9bdd809b56f2f196cf1a0ae315777
Kristina Kulabukhova <Kulabukhova.KA@phystech.edu>
2020-11-12 21:16:42 +0300

    finish

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-clear && g++ -std=c++20 main.cpp automate.cpp automate_builder.cpp -g $1 -o main && ./main && dot -Tpng dump.dot > dump.png && feh dump.png
+clear && g++ -std=c++20 main.cpp automate.cpp automate_builder.cpp -g $1 -o main && ./main 
